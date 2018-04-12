7e78ed4a8ba3b1cb8d81461c7e7e19952a2343bb
Rashmi <rashmis@tekvity.com>
2018-04-12 15:43:53 +0530

    registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule
